2057b4b1577adf1aa804613e529592f9b786babc
xumeng <xumeng126621@sina.cn>
2020-07-30 14:56:59 +0800

    生成dao文件

---
+++ hello-spring-boot-thymeleaf/src/main/java/com/ccb/hello/spring/boot/thymeleaf/dao/TesttableMapper.java
@@ -0,0 +1,8 @@
+package com.ccb.hello.spring.boot.thymeleaf.dao;
+
+import com.ccb.hello.spring.boot.thymeleaf.entity.Testtable;
+import tk.mybatis.MyMapper;
+
+public interface TesttableMapper extends MyMapper<Testtable> {
+
+}
