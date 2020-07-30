34988018cdee2c72176a69de74c231f705171913
xumeng <xumeng126621@sina.cn>
2020-07-30 15:00:42 +0800

    扫描mybits

--- hello-spring-boot-thymeleaf/src/main/java/com/ccb/hello/spring/boot/thymeleaf/HelloSpringBootThymeleafApplication.java
+++ hello-spring-boot-thymeleaf/src/main/java/com/ccb/hello/spring/boot/thymeleaf/HelloSpringBootThymeleafApplication.java
@@ -2,7 +2,9 @@ package com.ccb.hello.spring.boot.thymeleaf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 
+@MapperScan(basePackages = "com.ccb.hello.spring.boot.thymeleaf.dao")
 @SpringBootApplication
 public class HelloSpringBootThymeleafApplication {
 
