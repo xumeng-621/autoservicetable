8ea2a9c8b505c229c9f9d21b627070b60b6cc072
xumeng <xumeng126621@sina.cn>
2020-07-30 15:06:45 +0800

    测试mybits整合

--- hello-spring-boot-thymeleaf/src/test/java/com/ccb/hello/spring/boot/thymeleaf/HelloSpringBootThymeleafApplicationTests.java
+++ hello-spring-boot-thymeleaf/src/test/java/com/ccb/hello/spring/boot/thymeleaf/HelloSpringBootThymeleafApplicationTests.java
@@ -1,13 +1,27 @@
 package com.ccb.hello.spring.boot.thymeleaf;
 
+import com.ccb.hello.spring.boot.thymeleaf.dao.TesttableMapper;
+import com.ccb.hello.spring.boot.thymeleaf.entity.Testtable;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
-@SpringBootTest
-class HelloSpringBootThymeleafApplicationTests {
+import java.util.List;
 
-    @Test
-    void contextLoads() {
-    }
+@SpringBootTest(classes = HelloSpringBootThymeleafApplication.class)
+@Transactional
+@Rollback
+public class HelloSpringBootThymeleafApplicationTests {
+   @Autowired
+   private TesttableMapper testtableMapper;
+   @Test
+    public void testSelect(){
+       List<Testtable> testtables = testtableMapper.selectAll();
+       for (Testtable tt :testtables){
+           System.out.println(tt.getDeplymentname()+">>>>>>>>>>"+tt.getSystemname());
+       }
+   }
 
 }
