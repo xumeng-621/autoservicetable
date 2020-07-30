95c3695380d0ee7ba9aa206f8faee55ba0df3ff6
xumeng <xumeng126621@sina.cn>
2020-07-30 14:57:57 +0800

    消除无用的代码

--- hello-spring-boot-thymeleaf/src/main/java/com/ccb/hello/spring/boot/thymeleaf/controller/MainController.java
+++ hello-spring-boot-thymeleaf/src/main/java/com/ccb/hello/spring/boot/thymeleaf/controller/MainController.java
@@ -1,6 +1,6 @@
 package com.ccb.hello.spring.boot.thymeleaf.controller;
 
-import com.ccb.hello.spring.boot.thymeleaf.entity.User;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class MainController {
     @RequestMapping(value ={"","index"},method = RequestMethod.GET)
     public String index(Model model){
-        User user = new User();
-        user.setUsername("William");
-        model.addAttribute("user",user);
+
         return "index";
     }
 }
