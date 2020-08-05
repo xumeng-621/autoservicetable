cf8c24486029854da94ad6b6842fe4f22d49865e
yuluyaojh <353959938@qq.com>
2020-08-05 15:23:07 +0800

    集成Swagger

--- hello-spring-boot-thymeleaf/src/main/java/com/ccb/hello/spring/boot/thymeleaf/dao/TesttableMapper.java
+++ hello-spring-boot-thymeleaf/src/main/java/com/ccb/hello/spring/boot/thymeleaf/dao/TesttableMapper.java
@@ -1,8 +1,13 @@
 package com.ccb.hello.spring.boot.thymeleaf.dao;
 
 import com.ccb.hello.spring.boot.thymeleaf.entity.Testtable;
+import org.apache.ibatis.annotations.Param;
 import tk.mybatis.MyMapper;
 
+import java.util.List;
+
 public interface TesttableMapper extends MyMapper<Testtable> {
+
+    List<Testtable> getTestTypeByNameIsDepname(@Param("deplymentname") String deplymentname);
 
 }
